67538efa28b50d282130c59601084d88ce65dceb
Pavan Kumar <pavan2.kumar@intel.com>
2022-02-04 22:27:37 +0530

    Initial commit

---
+++ Sonar.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+
+namespace Sonar.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
