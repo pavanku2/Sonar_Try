cadc0089faff60c08354804905d4789b444a5f85
Pavan Kumar <pavan2.kumar@intel.com>
2022-02-07 10:20:59 +0530

    added unit tests

--- Sonar.Test/UnitTest1.cs
+++ Sonar.Test/UnitTest1.cs
@@ -10,5 +10,51 @@ namespace Sonar.Test
         {
             Assert.True(true);
         }
+
+
+        [Theory]
+        [InlineData(3, 6, 9)]
+        [InlineData(4, 3, 7)]
+        [InlineData(-10, 10, 0)]
+        [InlineData(-10, -10, -20)]
+        public void Addition_ShouldCalculateSimpleValues(int num1, int num2, int expected)
+        {
+            // Action
+            WeatherForecast weatherForecast = new WeatherForecast();
+
+            int actual = weatherForecast.Add(num1, num2);
+
+            // Assertion
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(8, 4, 2)]
+        [InlineData(-9, 3, -3)]
+        public void Division_ShouldCalculateSimpleValues(double num1, double num2, double expected)
+        {
+            WeatherForecast weatherForecast = new WeatherForecast();
+
+            // Action
+            double actual = weatherForecast.Division(num1, num2);
+
+            // Assertion
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Division_ShouldDivideByZero()
+        {
+            WeatherForecast weatherForecast = new WeatherForecast();
+
+            // Instantiation
+            double expected = 0;
+
+            // Action
+            double actual = weatherForecast.Division(15, 0);
+
+            // Assertion
+            Assert.Equal(expected, actual);
+        }
     }
 }
--- Sonar/WeatherForecast.cs
+++ Sonar/WeatherForecast.cs
@@ -9,5 +9,32 @@ namespace Sonar
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public  double Subtraction(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public  double Multiplication(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public  double Division(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return num1 / num2;
+            }
+        }
     }
 }
