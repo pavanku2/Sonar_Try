75d33115642bd6e966b510705f5b3c40f513ae4b
Pavan Kumar <pavan2.kumar@intel.com>
2022-02-08 17:53:03 +0530

    added dummy condition

--- Sonar/MathsOperations.cs
+++ Sonar/MathsOperations.cs
@@ -3,7 +3,10 @@
     public class MathsOperations
     {
         public int Add(int x, int y)
-        {
+        {    if(x < 0 || y < 0)
+            {
+                return 0;
+            }
             return x + y;
         }
     }
